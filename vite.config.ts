5f4742c696350f32398184f5e755dc572d04e529
Dallas <71103219+LeleDallas@users.noreply.github.com>
2023-04-21 17:50:14 +0200

    chore: update test coverage

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    coverage: {
+      lines: 75,
+      branches: 75,
+      functions: 75,
+      statements: 75,
+      provider: "c8",
+      all: true,
+      include: ["src/**/*.tsx", "src/**/*.ts"],
+      exclude: ["src/main.tsx", "src/vite-env.d.ts"],
+      reporter: ['text', 'json-summary', 'json'],
+    },
+    globals: true,
+    environment: "jsdom",
+
+  },
 })
