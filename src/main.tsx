434dde5a9c329a1824693520d54017122a7f481f
Dallas <71103219+LeleDallas@users.noreply.github.com>
2023-04-29 18:31:41 +0200

    refactor: update root page

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import "antd/dist/reset.css";
+import { Provider } from 'react-redux'
+import { store } from './store';
+import { ConfigProvider } from 'antd';
+import en_US from 'antd/es/locale/en_US';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ConfigProvider locale={en_US}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </ConfigProvider>
 )
