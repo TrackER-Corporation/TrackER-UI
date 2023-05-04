57710eab8df4f93286364326659fa25612713e79
Dallas <71103219+LeleDallas@users.noreply.github.com>
2023-05-04 18:44:31 +0200

    test: building utils

---
+++ test/buildingsUtils.test.tsx
@@ -0,0 +1,133 @@
+import '@testing-library/jest-dom';
+import { expect, describe, vi } from 'vitest'
+import { message } from 'antd';
+import { addBuilding, handleCoords, onSelect } from '../src/buildingsUtils';
+import api from '../src/api';
+import { fetchBuildings } from '../src/reducers/buildings';
+
+const user = {
+    _id: "62bee981e63f093c813b8a02",
+    name: "Emanuele",
+    surname: "Dall'Ara",
+    email: "emanuele@dernetsoft.com",
+    password: "abcd",
+    token: "1212",
+    type: "Building"
+
+
+};
+
+describe('onSelect', () => {
+    test('should set address, lat, and lon correctly', () => {
+        const setAddress = vi.fn();
+        const setLat = vi.fn();
+        const setLon = vi.fn();
+        const options = [
+            { value: 'New York', props: { lat: 40.7128, lon: -74.006 } },
+            { value: 'Los Angeles', props: { lat: 34.0522, lon: -118.2437 } },
+        ];
+        onSelect('New York', options, setAddress, setLat, setLon);
+        expect(setAddress).toHaveBeenCalledWith('New York');
+        expect(setLat).toHaveBeenCalledWith(40.7128);
+        expect(setLon).toHaveBeenCalledWith(-74.006);
+    });
+});
+
+describe('handleCoords', () => {
+    test('should fetch and set options correctly', async () => {
+        global.fetch = vi.fn().mockResolvedValue({
+            json: () => ({
+                results: [
+                    { formatted: 'New York', place_id: 1 },
+                    { formatted: 'Los Angeles', place_id: 2 },
+                ],
+            }),
+        });
+        const setOptions = vi.fn();
+        await handleCoords('New York', setOptions);
+        expect(fetch).toHaveBeenCalledWith(
+            'https://api.geoapify.com/v1/geocode/search?text=New York&format=json&apiKey=811f21d574e34738a95aca463b9dfd74',
+            { method: 'GET' }
+        );
+        expect(setOptions).toHaveBeenCalledWith([
+            { label: 'New York', value: 'New York', key: 1, props: { formatted: 'New York', place_id: 1 } },
+            { label: 'Los Angeles', value: 'Los Angeles', key: 2, props: { formatted: 'Los Angeles', place_id: 2 } },
+        ]);
+    });
+});
+
+describe('addBuilding', () => {
+    test('should add building and fetch buildings correctly', async () => {
+        const setShow = vi.fn();
+        const dispatch = vi.fn();
+        const organizationId = [1, 2];
+        const data = {
+            name: 'Building A',
+            contact: 'John Doe',
+            address: 'New York',
+            sqft: '1000',
+            type: 'Office',
+            lat: 40.7128,
+            long: -74.006,
+            userId: 1,
+            organizationId: [1, 2],
+        };
+        api.buildings.addBuilding = vi.fn().mockResolvedValue({});
+        api.buildings.fetchBuildings = vi.fn().mockResolvedValue([]);
+        await addBuilding('Building A', 'John Doe', 'New York', '1000', 'Office', 40.7128, -74.006, [1, 2], user, setShow, dispatch);
+        expect(api.buildings.addBuilding).toHaveBeenCalledWith(data);
+        expect(api.buildings.fetchBuildings).toHaveBeenCalledWith(1);
+        expect(setShow).toHaveBeenCalledWith(false);
+        expect(dispatch).toHaveBeenCalledWith(fetchBuildings([]));
+    });
+
+    test('should show error message when form is not filled', async () => {
+        const setShow = vi.fn();
+        const dispatch = vi.fn();
+        const organizationId = [1, 2];
+        const data = {
+            name: '',
+            contact: '',
+            address: '',
+            sqft: '',
+            type: '',
+            lat: 0,
+            long: 0,
+            userId: 1,
+            organizationId: [1, 2],
+        };
+        api.buildings.addBuilding = vi.fn().mockResolvedValue({});
+        api.buildings.fetchBuildings = vi.fn().mockResolvedValue([]);
+        const messageErrorSpy = vi.spyOn(message, 'error');
+        await addBuilding('', '', '', '', '', 0, 0, [1, 2], user, setShow, dispatch);
+        expect(api.buildings.addBuilding).not.toHaveBeenCalled();
+        expect(api.buildings.fetchBuildings).not.toHaveBeenCalled();
+        expect(setShow).toHaveBeenCalledWith(true);
+        expect(messageErrorSpy).toHaveBeenCalledWith('Fill the form to submit a building');
+    });
+
+    test('should show error message when building is not added', async () => {
+        const setShow = vi.fn();
+        const dispatch = vi.fn();
+        const organizationId = [1, 2];
+        const data = {
+            name: 'Building A',
+            contact: 'John Doe',
+            address: 'New York',
+            sqft: '1000',
+            type: 'Office',
+            lat: 40.7128,
+            long: -74.006,
+            userId: 1,
+            organizationId: [1, 2],
+        };
+        api.buildings.addBuilding = vi.fn().mockRejectedValue({});
+        api.buildings.fetchBuildings = vi.fn().mockResolvedValue([]);
+        const messageErrorSpy = vi.spyOn(message, 'error');
+        await addBuilding('Building A', 'John Doe', 'New York', '1000', 'Office', 40.7128, -74.006, [1, 2], user, setShow, dispatch);
+        expect(api.buildings.addBuilding).toHaveBeenCalledWith(data);
+        expect(api.buildings.fetchBuildings).not.toHaveBeenCalled();
+        expect(setShow).toHaveBeenCalledWith(true);
+        expect(messageErrorSpy).toHaveBeenCalledWith('Building not created!');
+    });
+});
