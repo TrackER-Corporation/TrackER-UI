c0eff66c4069edae0d853498b282c305f6538941
Dallas <71103219+LeleDallas@users.noreply.github.com>
2023-05-05 15:10:37 +0200

    test: Avatar Drawer

--- test/Account/AvatarDrawer.test.tsx
+++ test/Account/AvatarDrawer.test.tsx
@@ -11,7 +11,7 @@ describe('AvatarDrawer component', () => {
     const user = {}
     const onClose = vi.fn()
 
-    it('renders the component with the correct text', () => {
+    it('renders the component with the correct avatar', () => {
         render(
             <Provider store={store}>
                 <AvatarDrawer user={user} visible={true} onClose={onClose} />
@@ -24,9 +24,9 @@ describe('AvatarDrawer component', () => {
         expect(screen.getByText('Change')).toBeInTheDocument();
         fireEvent.click(screen.getByText('Change'));
         fireEvent.click(screen.getByText('Cancel'));
-        fireEvent.click(screen.getByTestId('avatar1'));
-        fireEvent.click(screen.getAllByTestId('avatar2')[10]);
-        fireEvent.click(screen.getByTestId('avatar3'));
+        fireEvent.click(screen.getAllByTestId('avatar1')[0]);
+        fireEvent.click(screen.getAllByTestId('avatar2')[0]);
+        fireEvent.click(screen.getAllByTestId('avatar3')[0]);
     });
 
 });
