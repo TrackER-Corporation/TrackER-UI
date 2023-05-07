fcb43585affdfb485c3c2a0c8c39d6f9faaa8961
Dallas <71103219+LeleDallas@users.noreply.github.com>
2023-05-08 00:50:30 +0200

    test: update account props

--- test/Account/Account.test.tsx
+++ test/Account/Account.test.tsx
@@ -7,25 +7,39 @@ import { ConfigProvider } from 'antd';
 import { Provider } from 'react-redux';
 import { store } from '../../src/store';
 import { BrowserRouter } from 'react-router-dom';
+import { UserProps } from '../../src/types';
 
 describe('Account', () => {
-    const user = {
+    const user: UserProps = {
         name: 'John',
         surname: 'Doe',
         email: 'john.doe@example.com',
-        type: 'Building'
+        type: 'Building',
+        _id: "1",
+        password: "1",
+        token: ""
+
+    };
+    const user2: UserProps = {
+        name: 'John',
+        surname: 'Doe',
+        email: 'john.doe@example.com',
+        type: 'Vendor',
+        _id: "1",
+        password: "1",
+        token: ""
     };
     const avatar = '';
     const socket = {};
     const updateRoute = vi.fn()
 
-    it('renders the profile page title', () => {
+    it('renders the profile Buildings', () => {
         const { getByText, getByTestId } = render(
 
             <ConfigProvider>
                 <BrowserRouter>
                     <Provider store={store}>
-                        <Account updateRoute={updateRoute} user={user} avatar={avatar} socket={socket} />
+                        <Account updateRoute={updateRoute} user={user} avatar={avatar} />
                     </Provider>
                 </BrowserRouter>
             </ConfigProvider>
@@ -43,7 +57,37 @@ describe('Account', () => {
         expect(menu).toBeInTheDocument();
         fireEvent.click(getByTestId("icon1"))
         fireEvent.click(getByTestId("menu"))
+        fireEvent.click(getByText("Security Settings"))
+        fireEvent.click(getByText("Change Password"))
 
+    });
+
+    it('renders the Vendor profile View', () => {
+        const { getByText, getByTestId } = render(
+
+            <ConfigProvider>
+                <BrowserRouter>
+                    <Provider store={store}>
+                        <Account updateRoute={updateRoute} user={user2} avatar={avatar} />
+                    </Provider>
+                </BrowserRouter>
+            </ConfigProvider>
+        );
+        const titleElement = getByText("Home");
+        expect(titleElement).toBeInTheDocument();
+        const nameElement = getByText(/John Doe/i);
+        const emailElement = getByText(/john.doe@example.com/i);
+        expect(nameElement).toBeInTheDocument();
+        expect(emailElement).toBeInTheDocument();
+
+        const menu = getByText("Personal Information");
+        expect(menu).toBeInTheDocument();
+        fireEvent.click(menu)
+        expect(menu).toBeInTheDocument();
+        fireEvent.click(getByTestId("icon1"))
+        fireEvent.click(getByTestId("menu"))
+        fireEvent.click(getByText("Security Settings"))
+        fireEvent.click(getByText("Change Password"))
     });
 
 });
