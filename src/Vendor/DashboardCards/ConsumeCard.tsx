dbed82bbb1c9d86f4705ca41423969bfaa91c926
Dallas <71103219+LeleDallas@users.noreply.github.com>
2023-05-13 17:52:33 +0200

    refactor: update ConsumeCard view

--- src/Vendor/DashboardCards/ConsumeCard.tsx
+++ src/Vendor/DashboardCards/ConsumeCard.tsx
@@ -1,4 +1,4 @@
-import { Col, Row } from "antd";
+import { Col, Row, Tabs, TabsProps } from "antd";
 import moment from "moment";
 import { useState } from "react";
 import { useEffect } from "react";
@@ -6,69 +6,40 @@ import ReactApexChart from "react-apexcharts";
 import api from "../../api";
 import { ApexOptions } from "apexcharts";
 import { useAppSelector } from "../../hooks";
+import { sortDate } from "../../Consumer/utils";
+import { commonProps } from "../../globalUtils";
 
-const commonProps = {
-    stroke: {
-        curve: 'smooth',
-        width: 2,
-
-    },
-    xaxis: {
-        type: 'datetime',
-        tooltip: {
-            enabled: false
-        },
-        labels: {
-            show: true,
-            datetimeUTC: false,
-            datetimeFormatter: {
-                year: 'yyyy',
-                month: "MMM 'yy",
-                day: 'dd MMM',
-                hour: 'HH:mm',
-            },
-        },
-    },
-    tooltip: {
-        enabled: true,
-        followCursor: true,
-        theme: "light",
-        x: {
-            show: true,
-            format: "dd-MM-yyyy HH:mm"
-        },
-    },
-}
 
 const ConsumeCard = () => {
     const organization = useAppSelector((state) => state.organization.organization)
-    const [bills, setBills] = useState({})
     const [electric, setElectric] = useState({})
     const [gas, setGas] = useState({})
     const [water, setWater] = useState({})
 
     const fetchBills = (aggId: string) => {
         api.bills.getBillsByOrganizationIdAggregated(aggId).then((res) => {
-            setBills(res.aggregated)
             let electric: any = []
             let gas: any = []
             let water: any = []
             Object.values(res.aggregated).map((el: any) => {
                 electric.push({
-                    x: moment.utc(el.date).local().format(),
+                    x: new Date(el.date).getTime(),
                     y: el.electric === undefined ? null : el.electric
                 })
                 gas.push({
-                    x: moment.utc(el.date).local().format(),
+                    x: new Date(el.date).getTime(),
                     y: el.gas === undefined ? null : el.gas
                 })
                 water.push({
-                    x: moment.utc(el.date).local().format(),
+                    x: new Date(el.date).getTime(),
                     y: el.water === undefined ? null : el.water
                 })
             })
+
+            sortDate(water)
+            sortDate(gas)
+            sortDate(electric)
             electric = {
-                type: 'line',
                 name: "Electric",
                 data: electric
             }
@@ -94,60 +65,68 @@ const ConsumeCard = () => {
         fetchBills(organization._id)
     }, [organization])
 
-
-
-    const state: any = {
-        electricLine: [electric],
-        electricOption: {
-            chart: {
-                id: 'electric',
-                group: 'social',
-                type: 'line',
-                height: 160
-            },
-            colors: ['#00E396'],
-            ...commonProps,
+    const electricOption: ApexOptions = {
+        ...commonProps,
+        chart: {
+            type: 'line',
+            height: 160
         },
+        colors: ['#00E396'],
+    }
 
-        gasLine: [gas],
-        gasOption: {
-            chart: {
-                id: 'gas',
-                group: 'social',
-                type: 'line',
-                height: 160
-            },
-            colors: ['#546E7A'],
-            ...commonProps,
+    const gasOption: ApexOptions = {
+        ...commonProps,
+        chart: {
+            type: 'line',
+            height: 160
         },
+        colors: ['#546E7A'],
 
-        waterLine: [water],
-        waterOption: {
-            chart: {
-                id: 'water',
-                group: 'social',
-                type: 'area',
-                height: 160
-            },
-            colors: ['#008FFB'],
-            ...commonProps,
+    }
+
+    const waterOption: ApexOptions = {
+        ...commonProps,
+        chart: {
+            type: 'area',
+            height: 160
         },
-    };
+        colors: ['#008FFB'],
+    }
 
-
-
+    const items: TabsProps['items'] = [
+        {
+            key: "1",
+            label: "Electric Usage",
+            children: <Col span={24}>
+                <ReactApexChart options={electricOption} series={[electric]} type="line" height={460} />
+            </Col>
+        },
+        {
+            key: "2",
+            label: "Gas Usage",
+            children: <Col span={24}>
+                <ReactApexChart options={gasOption} series={[gas]} type="line" height={460} />
+            </Col>
+        },
+        {
+            key: "3",
+            label: "Water Usage",
+            children: <Col span={24}>
+                <ReactApexChart options={waterOption} series={[water]} type="line" height={460} />
+            </Col>
+        },
+    ]
 
     return (
-        <Row justify="space-between" align="middle">
-            <Col md={8} xs={24} sm={24}>
-                <ReactApexChart options={state.electricOption as ApexOptions} series={state.electricLine} type="line" height={460} />
-            </Col>
-            <Col md={8} xs={24} sm={24}>
-                <ReactApexChart options={state.gasOption as ApexOptions} series={state.gasLine} type="line" height={460} />
-            </Col>
-            <Col md={8} xs={24} sm={24}>
-                <ReactApexChart options={state.waterOption as ApexOptions} series={state.waterLine} type="line" height={460} />
-            </Col>
+        <Row justify="space-between" align="middle" >
+            <Tabs
+                size="small"
+                tabPosition="top"
+                type="card"
+                items={items}
+                defaultActiveKey="1"
+                style={{ width: "100%" }}
+            />
         </Row>
     )
 }
